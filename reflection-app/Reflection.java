d5166c4812d19b2b4fa110dd8a707aa683ffa865
AshishKokane <87131092+AshishKokane@users.noreply.github.com>
2022-06-21 15:16:43 +0530

    Update Reflection.java

--- reflection-app/Reflection.java
+++ reflection-app/Reflection.java
@@ -14,22 +14,7 @@ public class Reflection {
 
 			reflectTest.getClassMetaData(Class.forName(name));
 
-			// Class cname = Class.forName(name);
-			// Class supercl = cname.getSuperclass(); checking if super class present or not
-			// String modifiers = Modifier.toString(cname.getModifiers());
-			// if (modifiers.length() > 0)
-			// System.out.print(modifiers + " ");
-			// System.out.print("class " + name);
-			// if (supercl != null && supercl != Object.class)
-			// System.out.print(" extends " + supercl.getName());
-
-			// System.out.print("\n{\n"); calling ReflectionTest methods in this file
-			// System.out.println("--Methods--");
-			// System.out.println();
-			// ReflectionTest.printMethods(cname);
-			// System.out.println();
-			// ReflectionTest.printFields(cname);
-			// System.out.println("}");
+			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
